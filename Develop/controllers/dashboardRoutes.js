ba552241697d889722bf1a1710ce9081e9d02d40
Meljska-Fawn <meli.explores@gmail.com>
2023-04-10 13:51:05 -0700

    debugged routes and added commentData to seeds

--- Develop/controllers/dashboardRoutes.js
+++ Develop/controllers/dashboardRoutes.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// Get all post with comments
 router.get('/', withAuth, async (req, res) => {
     try {
-        const postsData = await Post.findAll(req.session.user.id, { attributes: [ 'title', 'content', 'date_created' ]},
+        const postsData = await Post.findAll(req.session.user.id, { attributes: [ 'id', 'title', 'content', 'date_created' ]},
             {
             include: [
                 {
@@ -28,6 +29,33 @@ router.get('/', withAuth, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findbyPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                }]
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id.' });
+            return;
+        }
+        const post = postData.get({ plain: true });
+        res.render('edit-post', {
+            post,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/new-post', withAuth, (req, res) => {
+    res.render('new-post', {logged_In: true})
 });
 
 module.exports = router;
--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -39,7 +39,7 @@ router.get('/signup', (req, res) => {
 });
 
 // create a route to get a single post
-router.get('/post/:id', async (req, res) => {
+router.get('/post/:id', withAuth, async (req, res) => {
     try {
         const postData = await Post.findbyPk(req.params.id, {
             include: [
@@ -64,29 +64,6 @@ router.get('/post/:id', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     };
-});
-
-router.get('/edit/:id', withAuth, async (req, res) => {
-    try {
-        const postData = await Post.findbyPk(req.params.id, {
-            include: [
-                {
-                    model: User,
-                    attributes: ['username'],
-                }]
-        });
-        if (!postData) {
-            res.status(404).json({ message: 'No post found with this id.' });
-            return;
-        }
-        const post = postData.get({ plain: true });
-        res.render('edit-post', {
-            post,
-            logged_in: req.session.logged_in
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
 });
 
 module.exports = router;
